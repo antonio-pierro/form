79b925973d56db301bcdb56829760f50e4197784
Antonio Pierro <antonio.pierro@gmail.com>
2017-04-24 16:15:31 +0200

    Added html form

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="vendors/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+<body>
+    <div class="jumbotron">
+      <div class="container">
+      <?php include('fragments/navbar.html'); ?>
+      <?php include('fragments/welcome.html'); ?>
+      </div>
+    </div>
+    <?php include('fragments/form.html'); ?>
+</div>
+</div>
+<script type="text/javascript" src="vendors/js/jquery.min.js"></script>
+<script type="text/javascript" src="vendors/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/fixtures.js"></script>
+</body>
+</html>
