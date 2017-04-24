acd142e6d202db531f341a1f8ca57783e287406f
Antonio Pierro <antonio.pierro@gmail.com>
2017-04-24 16:35:49 +0200

    renamed js script file

--- index.php
+++ index.php
@@ -19,6 +19,6 @@
 </div>
 <script type="text/javascript" src="vendors/js/jquery.min.js"></script>
 <script type="text/javascript" src="vendors/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/fixtures.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
