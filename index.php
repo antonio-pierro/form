f2c298c90a9c187296fdc25f9d7212dce61174fb
Antonio Pierro <antonio.pierro@gmail.com>
2017-04-26 16:02:57 +0200

    Added boootstrap-validor

--- index.php
+++ index.php
@@ -17,8 +17,9 @@
     <?php include('fragments/form.html'); ?>
 </div>
 </div>
-<script type="text/javascript" src="vendors/js/jquery.min.js"></script>
-<script type="text/javascript" src="vendors/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/main.js"></script>
+<script src="vendors/js/jquery.min.js"></script>
+<script src="vendors/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.5.3/js/bootstrapValidator.min.js"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
